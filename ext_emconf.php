88dcb2dbfa4f07a5eb669ba375cbe4ea22bde1eb
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-29 14:52:18 +0000

    0.0.3 - SVN release
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/autositemap/trunk@66450 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,39 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = array(
+    'title'         => 'Auto grouping sitemap and menu',
+    'description'   => 'Plugin for a sitemap and menu which groups the first four menus in first four columns and all others in a fifth column. Great menus will get a linebreak. Configuration is based on TypoScript HMENU.',
+    'category'      => 'plugin',
+    'shy'           => 0,
+    'version'       => '0.0.3',
+    'dependencies'  => '',
+    'conflicts'     => '',
+    'priority'      => '',
+    'loadOrder'     => '',
+    'module'        => '',
+    'state'         => 'alpha',
+    'uploadfolder'  => 0,
+    'createDirs'    => '',
+    'modify_tables' => '',
+    'clearcacheonload' => 0,
+    'lockType' => '',
+    'author' => 'Dirk Wildt (Die Netzmacher)',
+    'author_email' => 'http://wildt.at.die-netzmacher.de',
+    'author_company' => '',
+    'CGLcompliance' => '',
+    'CGLcompliance_note' => '',
+    'constraints' => array(
+        'depends' => array(
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+          'devlog' => '',
+        ),
+    ),
+    'suggests' => array(
+      'devlog',
+    ),
+);
+
+?>
