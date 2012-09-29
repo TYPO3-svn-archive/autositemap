9f6d0f161bc40b4e411f049be20961186d3e4281
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-09-29 23:27:25 +0000

    0.0.4 - Improvements
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/autositemap/trunk@66470 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_autositemap_pi1.php
+++ pi1/class.tx_autositemap_pi1.php
@@ -2067,6 +2067,8 @@ class tx_autositemap_pi1 extends tslib_pibase
         {
           $prompt = 'The sitemap code doesn\'t contains the marker "' . $hashMarkerOuter . '"!';
           t3lib_div::devlog(' [ERROR/HTML] '. $prompt, $this->extKey, 3 );
+var_dump( __METHOD__, __LINE__, $content );
+die( );
         }
         return $arr_return;
       }
@@ -2247,9 +2249,7 @@ class tx_autositemap_pi1 extends tslib_pibase
         if( $this->b_drs_error )
         {
           $prompt = 'The sitemap code doesn\'t contains the marker "' . $hashMarkerOuter . '"!';
-          t3lib_div::devlog(' [ERROR/HTML] '. $prompt, $this->extKey, 3 );
-var_dump( __METHOD__, __LINE__, $content );
-die( );
+          t3lib_div::devlog(' [WARN/HTML] '. $prompt, $this->extKey, 3 );
         }
         continue; 
       }
