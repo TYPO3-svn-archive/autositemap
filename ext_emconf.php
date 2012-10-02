31356859d0c18f2658929ad819ce62530f7fe861
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-10-02 19:21:22 +0000

    0.9.3 +++ Bugfix YAML typoscript
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/autositemap/trunk@66557 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "autositemap".
 #
-# Auto generated 02-10-2012 17:31
+# Auto generated 02-10-2012 21:19
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Plugin for a sitemap and menu which groups the first four menus in first four columns and all others in a fifth column. Great menus will get a linebreak. Configuration is based on TypoScript HMENU.',
 	'category' => 'plugin',
 	'shy' => 0,
-	'version' => '0.9.2',
+	'version' => '0.9.3',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -44,7 +44,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'suggests' => array(
 		'0' => 'devlog',
 	),
-	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"737c";s:21:"ext_conf_template.txt";s:4:"883d";s:12:"ext_icon.gif";s:4:"c888";s:17:"ext_localconf.php";s:4:"b001";s:14:"ext_tables.php";s:4:"e776";s:16:"locallang_db.xml";s:4:"73e4";s:14:"doc/manual.pdf";s:4:"838b";s:14:"doc/manual.sxw";s:4:"6880";s:17:"lib/locallang.xml";s:4:"8ba9";s:50:"lib/extmanager/class.tx_autositemap_extmanager.php";s:4:"d509";s:37:"lib/icons/die-netzmacher.de_200px.gif";s:4:"48b3";s:32:"pi1/class.tx_autositemap_pi1.php";s:4:"8f49";s:16:"pi1/flexform.xml";s:4:"063f";s:27:"pi1/flexform_sheet_sDEF.xml";s:4:"682d";s:17:"pi1/locallang.xml";s:4:"a480";s:26:"pi1/locallang_flexform.xml";s:4:"6b9e";s:33:"res/pi1/yaml/autositemap_yaml.css";s:4:"e9f7";s:24:"static/pi1/constants.txt";s:4:"4dec";s:20:"static/pi1/setup.txt";s:4:"af4b";s:29:"static/pi1/yaml/constants.txt";s:4:"d41d";s:25:"static/pi1/yaml/setup.txt";s:4:"5096";}',
+	'_md5_values_when_last_written' => 'a:21:{s:9:"ChangeLog";s:4:"a78b";s:21:"ext_conf_template.txt";s:4:"883d";s:12:"ext_icon.gif";s:4:"c888";s:17:"ext_localconf.php";s:4:"b001";s:14:"ext_tables.php";s:4:"e776";s:16:"locallang_db.xml";s:4:"73e4";s:14:"doc/manual.pdf";s:4:"838b";s:14:"doc/manual.sxw";s:4:"6880";s:17:"lib/locallang.xml";s:4:"8ba9";s:50:"lib/extmanager/class.tx_autositemap_extmanager.php";s:4:"d509";s:37:"lib/icons/die-netzmacher.de_200px.gif";s:4:"48b3";s:32:"pi1/class.tx_autositemap_pi1.php";s:4:"8f49";s:16:"pi1/flexform.xml";s:4:"063f";s:27:"pi1/flexform_sheet_sDEF.xml";s:4:"682d";s:17:"pi1/locallang.xml";s:4:"a480";s:26:"pi1/locallang_flexform.xml";s:4:"6b9e";s:33:"res/pi1/yaml/autositemap_yaml.css";s:4:"e9f7";s:24:"static/pi1/constants.txt";s:4:"4dec";s:20:"static/pi1/setup.txt";s:4:"af4b";s:29:"static/pi1/yaml/constants.txt";s:4:"d41d";s:25:"static/pi1/yaml/setup.txt";s:4:"7987";}',
 );
 
 ?>
